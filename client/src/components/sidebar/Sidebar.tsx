44f017d174c78ded45fda36aff6cdc0af3baea4a
Katteu <katheamari@gmail.com>
2024-04-30 20:33:46 +0800

    Modified Sidebar

--- client/src/components/sidebar/Sidebar.tsx
+++ client/src/components/sidebar/Sidebar.tsx
@@ -1,20 +1,52 @@
-import React from 'react'
+import React,{useEffect, useState} from 'react'
 import logo from "../../assets/logo.png"
-import { FaPlus } from "react-icons/fa";
+import { FaPlus,FaCircle } from "react-icons/fa";
+import { BsThreeDotsVertical } from "react-icons/bs";
 
 const Sidebar = () => {
+  //Pag set lang ug interface diri Tyrone ako rani gi static for now
+  const [catColor,setCatColor] = useState('');
+  
+  useEffect (()=>{
+    setCatColor('#FFB703')
+  },[])
+
   return (
     <div className='w-full h-full'>
         <div className='bg-[#001A27] h-[8vh] rounded-tr-3xl'>
             <img src={logo} alt="Produktib Logo" className="h-auto w-[15rem] py-[5%] pl-[10%]"></img>
         </div>
         <div className='bg-[#023047] h-[92vh]'>
-            <div className='mx-[10%] pt-[7%] flex'>
+            <div className='mx-[10%] pt-[7%] flex items-center'>
                 <h1 className='font-bold text-[#D3D3D3] text-[1.15em] mr-[7%]'>TASK CATEGORIES</h1>
-                <FaPlus className='text-[#D3D3D3] text-[1.15em] mt-[1.5%] hover:cursor-pointer hover:text-white'/>
+                <FaPlus className='text-[#D3D3D3] text-[1.15em] hover:cursor-pointer hover:text-white'/>
             </div>
-            
-            
+
+            {/* Category List */}
+            <div className='pt-[2%]'>
+                <ul>
+                    <li className='py-[2%] bg-[#085A83] hover:cursor-pointer'>
+                    <div className='flex items-center justify-between mx-[10%]'>
+                        <div className='flex items-center'>
+                            <FaCircle className='mr-[5%] text-[1.15em]' style={{ color: catColor }}/>
+                            <p className='text-[1.15em] text-white font-semibold'>Academics</p>
+                        </div>
+                        <BsThreeDotsVertical className='hover:animate-shake text-white'/>
+                    </div>
+                    </li>
+                    <li className='py-[2%] hover:cursor-pointer hover:bg-[#085A83]'>
+                    <div className='flex items-center justify-between mx-[10%]'>
+                        <div className='flex items-center'>
+                            <FaCircle className='mr-[5%] text-[1.15em] text-[#FB8500]'/>
+                            <p className='text-[1.15em] text-white font-semibold'>Work</p>
+                        </div>
+                        <BsThreeDotsVertical className='hover:animate-shake text-white'/>
+                    </div>
+                    </li>
+                </ul>
+                {/* Category Row */}
+
+            </div>
         </div>        
     </div>
   )
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -17,9 +17,23 @@ module.exports = {
             opacity: '1',
           },
         },
+
+        'shake': {
+          '0%, 100%': {
+            transform: 'translateX(0)',
+          },
+          '10%, 30%, 50%, 70%, 90%': {
+            transform: 'translateX(-2px)',
+          },
+          '20%, 40%, 60%, 80%': {
+            transform: 'translateX(2px)',
+          },
+        },
+
       },
       animation: {
         'fade-in': 'fade-in 0.5s ease-in-out',
+        'shake': 'shake 0.9s cubic-bezier(0.04, 0.04, 0.04, 0.04)',
       },
     },
   },
