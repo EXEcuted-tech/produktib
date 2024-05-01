89e1d8fa88cd0b63104d3b5afeef8acc09f2937a
FranzGwapo <franzcasimir.ondiano@gmail.com>
2024-05-01 18:20:06 +0800

    Manually Added Dark Mode rar

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,16 +1,37 @@
 import React from 'react';
 import './App.css';
+import { useState } from "react";
+import { FaMoon, FaRegMoon } from "react-icons/fa";
 import Sidebar from './components/sidebar/Sidebar'
+import Add from './components/modal/Add'
 
 //We will use this ra since usa ra ato page hehe
 
 function App() {
+  
+    const[darkMode, setDarkMode] = useState(false);
+
+    const toggleDarkMode = ( ) => {
+        setDarkMode(!darkMode);
+    }
+
   return (
-    <div className="animate-fade-in font-montserrat">
-      <div className='w-[14%]'>
-        <Sidebar/>
+    <div className={`${darkMode && "dark"}`}>
+      <div className="animate-fade-in font-montserrat flex">
+        <div className='w-[14%]'>
+          <Sidebar/>
+        </div>
+
+        {/* For the modals */}
+        <div className='fixed w-[100%]'>
+          <button className='float-right w-12 h-12 b-12 r-12 mt-[1%] ml-auto mr-[3%] rounded-full' onClick={toggleDarkMode}> 
+            {darkMode? <FaRegMoon className='ml-auto mr-auto w-8 h-8 b-8 r-8'/> : <FaMoon className='ml-auto mr-auto w-8 h-8 b-8 r-8'/>}
+          </button>
+          {/* <Add/> */}
+        </div>
       </div>
     </div>
+    
   );
 }
 
--- client/src/components/modal/Add.tsx
+++ client/src/components/modal/Add.tsx
@@ -1,15 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { FaT, FaAlignLeft, FaAlignJustify } from "react-icons/fa6";
 
 
 const Add = () => {
+
     return(
         <div className='fixed w-full h-full overflow-auto text-2xl z-2 backdrop-blur-sm'>
             <div className='w-[50%] h-[60%] ml-[33%] mr-auto mt-[10%] mb-auto rounded-2xl z-1'>
-                <div className='flex w-full h-[20%] bg-[#023047] items-center rounded-2xl rounded-b-none'>
+                <div className='flex w-full h-[20%] bg-[#023047] items-center rounded-2xl rounded-b-none dark:bg-black'>
                     <p className='text-white ml-[6%] text-4xl font-semibold '>Add Task</p>
                 </div>
-                <div className='flex w-full h-[80%] bg-[#F1F1F1] rounded-2xl rounded-t-none'>
+                <div className='flex w-full h-[80%] bg-[#F1F1F1] rounded-2xl rounded-t-none dark:border dark:border-black'>
                     <form className='w-[83%] ml-[8%] mt-[5%] mb-[3%]'>
                         <div className='mb-5'>
                             <div className='flex mb-2'>
@@ -17,18 +18,18 @@ const Add = () => {
                                 <FaAlignLeft className='text-sm mt-[1.4%] mb-auto ml-[-1%]'/>
                                 <p className='ml-[1%] text-2xl font-semibold '>Title</p>
                             </div>
-                            <input type='text' className='w-[100%] p-4 pl-5 rounded-lg bg-[#FAFAFA] placeholder-gray-500 placeholder:font-bold font-bold border-[#FFB703] border-2' placeholder="Enter Task Title" ></input>
+                            <input type='text' className='w-[100%] p-4 pl-5 rounded-lg bg-[#FAFAFA] placeholder-gray-500 placeholder:font-bold font-bold border-[#FFB703] border-2 dark:border-black' placeholder="Enter Task Title" ></input>
                         </div>
                         <div className='mb-5'>
                             <div className='flex mb-2'>
                                 <FaAlignJustify className='text-xl mt-auto mb-auto'/>
                                 <p className='ml-[2%] text-2xl font-semibold'>Description</p>
                             </div>
-                            <textarea id='description' className='w-full h-[8rem] p-2 pl-5 resize-none text-xl rounded-lg border-[#FFB703] border-2' placeholder="Enter Task Description"></textarea>
+                            <textarea id='description' className='w-full h-[8rem] p-2 pl-5 resize-none text-xl rounded-lg border-[#FFB703] border-2 dark:border-black' placeholder="Enter Task Description"></textarea>
                         </div>
                         <div className='flex justify-end'>
-                            <button className="text-white bg-[#D6D6D6] text-[#023047] text-2xl font-bold py-2.5 px-2.5 rounded">Cancel</button>
-                            <button type="submit" className="text-white ml-[3%] bg-[#FB8500] font-semibold py-2.5 px-6 rounded">Save</button>
+                            <button className="bg-[#D6D6D6] text-[#023047] text-2xl font-bold py-2.5 px-2.5 rounded dark:border-black dark:bg-white dark:text-black">Cancel</button>
+                            <button type="submit" className="text-white ml-[3%] bg-[#FB8500] font-semibold py-2.5 px-6 rounded dark:bg-black">Save</button>
                         </div>
 
                     </form>
