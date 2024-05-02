b9a40763560a921b0cd643a0afba75b5652a5105
Katteu <katheamari@gmail.com>
2024-05-03 04:28:50 +0800

    Modified changes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,23 @@
 import React from 'react';
 import './App.css';
 import Sidebar from './components/sidebar/Sidebar'
+import Header from './components/header/Header';
 
 //We will use this ra since usa ra ato page hehe
 
 function App() {
   return (
     <div className="animate-fade-in font-montserrat">
-      <div className='w-[14%]'>
-        <Sidebar/>
+      <div className='flex'>
+        <div className='w-[14%]'>
+          <Sidebar/>
+        </div>
+        <div>
+          <Header/>
+          <div className='bg-[#F3F3F3] w-[100%] h-[93vh]'>
+            TEST
+          </div>
+        </div>
       </div>
     </div>
   );
---
+++ client/src/components/header/Header.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div>
+        <h1 className='ml-[20%] py-[5%] font-black text-[2em]'>Academics</h1>
+        
+    </div>
+  )
+}
+
+export default Header
