125ad47f4c1ca45e8e91240ce98b7be6ea033a1a
Angelou Sereño <angeloubendebelsereno@gmail.com>
2024-05-05 04:03:41 +0800

    delete function

--- api/controllers/catController.js
+++ api/controllers/catController.js
@@ -10,7 +10,21 @@ const updateCategory = (req,res)=>{
 }
 
 const deleteCategory = (req,res)=>{
-    
+  const {val} = req.query;
+  const query = `DELETE FROM category WHERE id = ?`
+
+  db.query(query,[val],(err, rows)=>{
+    if(err){
+      console.error(`Error deleting record with ID:${val}`, err);
+      return res.status(500).json({status: 500, success:false, error: 'Error Deleting Category'});
+    }else{
+      return res.status(200).json({
+        status: 200,
+        success: true,
+        message: 'Successfully Deleted Category'
+      })
+    }
+  })
 }
 
 const retrieveAll = (req,res)=>{   
--- api/controllers/taskController.js
+++ api/controllers/taskController.js
@@ -10,7 +10,21 @@ const updateTask = (req,res)=>{
 }
 
 const deleteTask = (req,res)=>{
-    
+  const {val} = req.query;
+  const query = `DELETE FROM task WHERE id = ?`
+
+  db.query(query,[val],(err, rows)=>{
+    if(err){
+      console.error(`Error deleting record with ID:${val}`, err);
+      return res.status(500).json({status: 500, success:false, error: 'Error Deleting Task'});
+    }else{
+      return res.status(200).json({
+        status: 200,
+        success: true,
+        message: 'Successfully Deleted Task'
+      })
+    }
+  })
 }
 
 const retrieveAll = (req,res)=>{   
